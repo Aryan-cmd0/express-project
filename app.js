c124c0883c1c3d4eec1857d9fdf2b1df385454f4
Aryan-cmd0 <aryan889pati@gmail.com>
2026-03-03 15:29:26 +0530

    Built first Express server with dynamic route

---
+++ app.js
@@ -0,0 +1,6 @@
+const greet = require("./utils/greet")
+const math = require("./utils/math")
+
+console.log(greet("Aryan"));
+console.log(math.add(0,6));
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("My first Express server 🚀");
+});
+
+
+app.get("/user/:name",(req,res)=>{
+    console.log("User route hit")
+    res.send(`Hello ${req.params.name}👋 `)
+})
+
+app.listen(3000, () => {
+    console.log("Server running at https://localhost:3000");
+});
